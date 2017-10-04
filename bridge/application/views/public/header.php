e6d0c4e9cb7bafe8b1c3eb9cae80f2db249a64c3
elddenmedio <elddenmedio@gmail.com>
2017-10-04 11:38:40 -0500

    Add files via upload

---
+++ bridge/application/views/public/header.php
@@ -0,0 +1,51 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Easy Admin Panel an Admin Panel Category Flat Bootstrap Responsive Website Template | Sign In :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Easy Admin Panel Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="../../resources/css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="../../resources/css/style.css" rel='stylesheet' type='text/css' />
+<!-- Graph CSS -->
+<link href="../../resources/css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<!-- lined-icons -->
+<link rel="stylesheet" href="../../resources/css/icon-font.min.css" type='text/css' />
+<!-- //lined-icons -->
+<!-- chart -->
+<script src="../../resources/js/Chart.js"></script>
+<!-- //chart -->
+<!--animate-->
+<link href="../../resources/css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="../../resources/js/wow.min.js"></script>
+
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+<!----webfonts--->
+<link href='//fonts.googleapis.com/css?family=Cabin:400,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+<!---//webfonts---> 
+ <!-- Meters graphs -->
+<script src="../../resources/js/jquery-1.10.2.min.js"></script>
+<!-- Placed js at the end of the document so the pages load faster -->
+
+
+<style>
+    .has-error{ border-color: #a94442; -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075); box-shadow: inset 0 1px 1px rgba(0,0,0,.075);}
+    #loading {display: none; background-color: rgba(255,255,255,0.7); background-image: url(../../resources/images/loading.gif); background-repeat: no-repeat; background-position: center; background-size: auto; position: absolute; z-index: 999999; width: 100%; height: 100%; top: 0;}
+</style>
+</head> 
+   
+ <body class="sign-in-up">
---
+++ bridge/application/views/public/index.php
@@ -0,0 +1,36 @@
+
+    <section>
+			<div id="page-wrapper" class="sign-in-wrapper">
+				<div class="graphs">
+					<div class="sign-in-form">
+						<div class="sign-in-form-top">
+							<!--<p><span>Sign In to</span> <a href="index.html">Admin</a></p>-->
+						</div>
+						<div class="signin">
+							<div class="signin-rit">
+									 <!--<label class=""><a href="">Forgot Password ?</a></label>-->
+								<div class="clearfix"> </div>
+							</div>
+							<form id="login">
+                                                            <div class="log-input" id="f-email">
+								<div class="log-input-left">
+                                                                    <input type="email" id="email" name="usr" class="user">
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+                                                            <div class="log-input" id="f-psw">
+								<div class="log-input-left">
+                                                                    <input type="password" id="psw" name="psw" class="lock">
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<input type="submit" value="Login to your account" id="submit-info">
+						</form>	 
+						</div>
+						<div class="new_people">
+							<!--<a href="sign-up.html">Register Now!</a>-->
+						</div>
+					</div>
+				</div>
+			</div>
+		
