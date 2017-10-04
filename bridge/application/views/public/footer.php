70151552f6a1a324e2ce34885184e937444894d4
elddenmedio <elddenmedio@gmail.com>
2017-10-04 11:26:21 -0500

    Create footer.php

---
+++ bridge/application/views/public/footer.php
@@ -0,0 +1,24 @@
+
+        <!--footer section start-->
+			<footer>
+			   <p>&copy 2017 Easy Admin Panel. All Rights Reserved | Design by <a href="http://elddenmedio.site/" target="_blank">Eldd-System.</a></p>
+			</footer>
+        <!--footer section end-->
+	</section>
+        
+        <div id="loading" style="display: none;"></div>
+	
+<script src="../../resources/js/jquery.nicescroll.js"></script>
+<script src="../../resources/js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+   <script src="../../resources/js/bootstrap.min.js"></script>
+   
+   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        
+        <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js'></script>
+        
+        <script src="../../resources/js/sienity.min.js"></script>
+</body>
+</html>
